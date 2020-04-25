90a7a3f5fd9406b85dcbfb14b13637b1ffb69f83
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-04-24 22:49:05 -0700

    first question

--- 04-Homework/script.js
+++ 04-Homework/script.js
@@ -1,3 +1,16 @@
+var questions = [ 
+  {
+    question: "What is Millie's favorite food?",
+    answers: [ 
+      {text: "Hot Dogs", correct: false},
+      {text: "Flowers", correct: false},
+      {text: "Dog Food", correct: false},
+      {text: "Cookies", correct: true},
+    ],
+    class: ["dogs", "flowers", "food", "cookies"],
+  }
+]
+
 
 $( document ).ready(function() {
   // Start 
@@ -5,9 +18,28 @@ $( document ).ready(function() {
   $("#question").text("Do you think you know Millie?");
   // Hide Answer Buttons
   $("#answer-buttons").hide();
-  // On Click Start Questions
-  
+  // On Click Start Questions & timer
+  $("#start-btn").on("click", function() {
+    // hide start button 
+    $("#start-btn").hide();
+    questionOne();
+    // timer();
+    $("#answer-buttons").show();
+  })
+  debugger;
+  function questionOne() {
+    // Change Question Text
+    $("#question").text(questions[0].question);
+    // Change Answer Button Text
+    $("#answer0").text(questions[0].answers[0].text).addClass(questions[0].class[0]);
+    $("#answer1").text(questions[0].answers[1].text).addClass(questions[0].class[1]);
+    $("#answer2").text(questions[0].answers[2].text).addClass(questions[0].class[2]);
+    $("#answer3").text(questions[0].answers[3].text).addClass(questions[0].class[3]);
+    // Change Answer Button Images
+    // On Click Right 
+    // On Click Wrong 
+  }
 
-  
-  
+
+
 });
