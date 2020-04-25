ae908b114185704a59eacd33113b1822799cbc9a
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-04-24 23:18:40 -0700

    added for loop for buttons

--- 04-Homework/script.js
+++ 04-Homework/script.js
@@ -8,6 +8,26 @@ var questions = [
       {text: "Cookies", correct: true},
     ],
     class: ["dogs", "flowers", "food", "cookies"],
+  },
+  {
+    question: "Where is Millie's favorite place to be?",
+    answers: [ 
+      {text: "Park", correct: true},
+      {text: "Beach", correct: false},
+      {text: "Home", correct: false},
+      {text: "Snow", correct: false},
+    ],
+    class: ["park", "beach", "home", "snow"],
+  },
+  {
+    question: "Where is Millie's favorite hiking spot?",
+    answers: [ 
+      {text: "Annie's Canyon", correct: true},
+      {text: "Cowel's Mountain", correct: false},
+      {text: "Mission Trails", correct: false},
+      {text: "Fiesta Island", correct: false},
+    ],
+    class: ["annies", "cowels", "trails", "island"],
   }
 ]
 
@@ -26,20 +46,49 @@ $( document ).ready(function() {
     // timer();
     $("#answer-buttons").show();
   })
-  debugger;
+
   function questionOne() {
     // Change Question Text
     $("#question").text(questions[0].question);
     // Change Answer Button Text
-    $("#answer0").text(questions[0].answers[0].text).addClass(questions[0].class[0]);
-    $("#answer1").text(questions[0].answers[1].text).addClass(questions[0].class[1]);
-    $("#answer2").text(questions[0].answers[2].text).addClass(questions[0].class[2]);
-    $("#answer3").text(questions[0].answers[3].text).addClass(questions[0].class[3]);
+
+    for (var i = 0; i < 4; i++) {
+      $("#answer" + i).text(questions[0].answers[i].text).addClass(questions[0].class[i]);
+    }
     // Change Answer Button Images
     // On Click Right 
     // On Click Wrong 
-  }
+    $("#answer-buttons").on("click", function() {
+      questionTwo();
+    });
+  };
 
-
-
+  function questionTwo() {
+    // Change Question Text
+    $("#question").text(questions[1].question);
+    // Change Answer Button Text
+    for (var i = 0; i < 4; i++) {
+      $("#answer" + i).text(questions[1].answers[i].text).addClass(questions[1].class[i]);
+    }
+    // Change Answer Button Images
+    // On Click Right 
+    // On Click Wrong 
+    $("#answer-buttons").on("click", function() {
+      questionThree();
+    });
+  };
+  
+  function questionThree() {
+    // Change Question Text
+    $("#question").text(questions[2].question);
+    for (var i = 0; i < 4; i++) {
+      $("#answer" + i).text(questions[2].answers[i].text).addClass(questions[2].class[i]);
+    }
+    // Change Answer Button Images
+    // On Click Right 
+    // On Click Wrong 
+    $("#answer-buttons").on("click", function() {
+      
+    });
+  };
 });
