0615dc61e8289a440dde14f09e4580e87ffce9bb
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-04-24 21:54:52 -0700

    started js

--- 04-Homework/script.js
+++ 04-Homework/script.js
@@ -1,4 +1,13 @@
-// Start 
-// Need Start Button 
-// Question: Display do you think you know millie
 
+$( document ).ready(function() {
+  // Start 
+  // Intro Text
+  $("#question").text("Do you think you know Millie?");
+  // Hide Answer Buttons
+  $("#answer-buttons").hide();
+  // On Click Start Questions
+  
+
+  
+  
+});
